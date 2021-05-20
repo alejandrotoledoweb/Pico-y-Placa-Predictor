8b3577071cbce9e4b7d8b7eee7ae62cdb3d7488d
alejandrotoledoweb <atoledofr@gmail.com>
2021-05-20 12:24:52 -0500

    add style.css file and checkings.js file with checking function and const for the data from the html form

---
+++ js/checkings.js
@@ -0,0 +1,41 @@
+const totalInfo = []
+const placa = document.querySelector('#placa')
+const dia = document.querySelector('#dia')
+const hora = document.querySelector('#hora')
+const btn = document.querySelector('#revisar')
+const lastNumber = placa.slice(6, 7)
+
+const time1 = '06:00';
+const time2 = '20:00';
+
+export { placa, dia, hora, btn, lastNumber };
+
+export function saveInfo() {
+  localStorage.setItem('totalInfo', JSON.stringify(totalInfo));
+}
+
+export function checkPlacaAndDay(lastNumber, day) {
+  if (lastNumber >= 0 && lasNumber <= 3 && day == 'lunes') {
+    return true 
+  } else if (lastNumber >= 2 && lasNumber <= 4 && day == 'martes') {
+    return true 
+  } else if (lastNumber >= 4 && lasNumber <= 7 && day == 'miercoles') {
+    return true 
+  } else if (lastNumber >= 6 && lasNumber <= 9 && day == 'jueves') {
+    return true 
+  } else if (lastNumber >= 0 && lasNumber <= 1 && lasNumber <= 9 && lasNumber >= 8 && day == 'viernes'){
+    return true 
+  } else if (day == 'sabado' && day == 'domingo') {
+    return true
+  } else {
+    return false
+  }
+}
+
+export function checkHour(hour) {
+  if (hour.target.textContent >= 'Read') {
+    hour.target.textContent = 'Not Read';
+  } else {
+    hour.target.textContent = 'Read';
+  }
+}
